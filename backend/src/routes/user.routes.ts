13cc05628716e33ad9bc425c88839890618bd19a
shambel96 <shambeldechu@gmil.com>
2025-10-17 02:59:10 +0300

    feat(auth): add change password api endpoint

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,11 @@
-import { createUsers, login } from "../controllers/user.controller";
+import { createUsers, login, logout } from "../controllers/user.controller";
 import { Router, Request, Response } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { changePassword } from "../controllers/user.controller";
 const router = Router();
 
 router.post("/register", createUsers);
 router.post("/login", login);
-
+router.post("/logout", authMiddleware, logout); // <-- logout route (protected)
+router.put("/change-password", changePassword);
 export default router;
