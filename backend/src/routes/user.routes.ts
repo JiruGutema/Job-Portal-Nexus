e6e583ca66aeec0024303aa5db83f49289ac0865
shambel96 <shambeldechu@gmil.com>
2025-10-18 00:50:19 +0300

    feat(routes): add get loggedIn user route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,9 +4,11 @@ import {
   logout,
   changePassword,
   deleteAccount,
+  getLoggedInUser,
 } from "../controllers/user.controller";
 import { Router, Request, Response } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { get } from "http";
 const router = Router();
 
 router.post("/register", createUsers);
@@ -14,4 +16,5 @@ router.post("/login", login);
 router.post("/logout", authMiddleware, logout); // <-- logout route (protected)
 router.put("/change-password", changePassword);
 router.delete("/delete-account/:id", deleteAccount);
+router.get("/me", authMiddleware, getLoggedInUser);
 export default router;
